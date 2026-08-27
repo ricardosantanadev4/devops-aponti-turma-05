4a4725d1396c54334b524fcd70449e6d31aa81f8
ricardosantanadev4 <ricardosantanadev4@zarpar.app>
2026-08-27 14:11:06 -0300

    feat: adiciona arquivos outputs.tf e variables.tf

--- terraform/provisionamento-automático/outputs.tf
+++ terraform/provisionamento-automático/outputs.tf
@@ -1,1 +1,1 @@
-# Este arquivo define as saídas do Terraform para o projeto
+# Este arquivo é utiçizado para exibir valores importantes que foram gerados ou obtidos durante a execução do Terraform
---
+++ terraform/provisionamento-automático/variables.tf
@@ -0,0 +1,1 @@
+#  Neste arquivo são declaradas as variáveis que serão utilizadas na configuração do Terraform.
